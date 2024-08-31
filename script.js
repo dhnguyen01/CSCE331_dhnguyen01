cac2eb255884980aab90f5c19dca2b180e5c88f3
dhnguyen01 <112366465+dhnguyen01@users.noreply.github.com>
2024-08-31 15:54:08 -0500

    added to portfolio and changed script code

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-document.addEventListener('mousemove', function(e) {
-    const offsetX = e.clientX - 950;
-    const offsetY = e.clientY - 480 + window.scrollY; 
-    document.body.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
+document.addEventListener("mousemove", function (e) {
+  const offsetX = e.clientX - 950;
+  const offsetY = e.clientY - 480 + window.scrollY;
+  document.body.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var themeStyle = document.getElementById("themeStyle");
+  var storedTheme = localStorage.getItem("themeStyle");
+
+  if (storedTheme) {
+    themeStyle.setAttribute("href", storedTheme);
+  }
+
+  var button = document.getElementById("styleButton");
+  if (button) {
+    button.addEventListener("click", function () {
+      var newTheme =
+        themeStyle.getAttribute("href") === "style1.css"
+          ? "style2.css"
+          : "style1.css";
+      themeStyle.setAttribute("href", newTheme);
+
+      localStorage.setItem("themeStyle", newTheme);
+    });
+  }
 });
